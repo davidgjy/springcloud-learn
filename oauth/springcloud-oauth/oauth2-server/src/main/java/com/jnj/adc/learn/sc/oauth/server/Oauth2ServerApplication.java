446ecaed48cee74a312807bdc718ebbf6dcc7594
Kelvin Gu <jgu55@its.jnj.com>
2020-03-09 22:13:53 +0800

    sc-oauth

---
+++ oauth/springcloud-oauth/oauth2-server/src/main/java/com/jnj/adc/learn/sc/oauth/server/Oauth2ServerApplication.java
@@ -0,0 +1,17 @@
+package com.jnj.adc.learn.sc.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : KG
+ * description:
+ * create date: 9:54 PM 2020/3/9
+ * modified by:
+ */
+@SpringBootApplication
+public class Oauth2ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerApplication.class, args);
+    }
+}
